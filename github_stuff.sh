958b2d1ce3cf38a9cf56b0fadfa919c32ffec45a
darcyj <jack.l.darcy@gmail.com>
2019-06-17 14:58:49 -0600

    17jun2019

--- github_stuff.sh
+++ github_stuff.sh
@@ -1,4 +1,11 @@
-# git init # run once
+# run once:
+# git init 
+# git add infant_gut_analysis/ finnish_infants_analysis/ moving_pix_analysis/ drafts/ R_functions/ readme.md github_stuff.sh 
+# git commit -m "first commit"
+# git remote add origin https://github.com/darcyj/pd_model.git
+# git push -u origin master
+
+# run to update
 git add infant_gut_analysis/ finnish_infants_analysis/ moving_pix_analysis/ drafts/ R_functions/ readme.md github_stuff.sh 
 git commit -m 17jun2019
 git push origin master
